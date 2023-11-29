d9ea6d21b971b5f95ad7e4ac8ae5875dc6a1e069
ytfang <139977950+YitianFang@users.noreply.github.com>
2023-11-30 00:03:14 +0800

    Update main.py

--- DeepProSite-main/main.py
+++ DeepProSite-main/main.py
@@ -23,7 +23,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset_path", type=str, default='./datasets/')
 parser.add_argument("--feature_path", type=str, default='./feature/')
-parser.add_argument("--task", type=str, default='Metal') # PRO CA MG MN Metal
+parser.add_argument("--task", type=str, default='PRO') # PRO CA MG MN Metal
 parser.add_argument("--num_workers", type=int, default=8)
 parser.add_argument("--train", action='store_true', default=False)
 parser.add_argument("--test", action='store_true', default=False)
@@ -66,8 +66,8 @@ for pdb_id in ID_list:
         all_protein_data[pdb_id]=torch.load(root+f"{pdb_id}_X.tensor"),torch.load(root+f"{pdb_id}_node_feature.tensor"),torch.load(root+f"{pdb_id}_mask.tensor") # ,torch.load(root+f"{pdb_id}_label.tensor")
 
 
-train_size = {"PRO":335, "CA":1550, "MG":1729, "MN":547, "Metal":5469}  #######################3
-num_samples = train_size[task] * 5 # 1个epoch等于5个
+train_size = {"PRO":335, "CA":1550, "MG":1729, "MN":547, "Metal":5469}  
+num_samples = train_size[task] * 5 
 
 nn_config = {
     'node_features': 1024 + 14, # ProtTrans + DSSP
@@ -77,8 +77,8 @@ nn_config = {
     'k_neighbors': 30,
     'augment_eps': 0.1,
     'dropout': 0.3,
-    'id_name':'ID', # column name in dataframe
-    'obj_max': 1,   # optimization object: max is better
+    'id_name':'ID',
+    'obj_max': 1,
     'epochs': 30,
     'patience': 8,
     'batch_size': 32,
